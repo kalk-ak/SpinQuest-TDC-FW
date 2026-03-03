44cfc5551eb6adabdbb6226fd437c1be562ba7b7
Kaleb Aklilu <kalebaklilu3@gmail.com>
2026-03-02 23:23:19 -0500

    Added documentations for the constructor and run spill in fake fpga client. Also deleated estabilish connection because we can just use the already defined utility that creates connection (found in defined_utilities.h)

--- kaleb-projects/include/fake_fpga_client.h
+++ kaleb-projects/include/fake_fpga_client.h
@@ -15,26 +15,48 @@ class FakeFPG
     // and speed (sleep time between sends)
   public:
     // constructor
-    FakeFPG(const std::string &ip, int port, const std::string &proto = "tcp",
-            int internet_type = AF_INET, double spill_duration_sec = 4.0,
+    /**
+     * @brief Constructs a Fake FPGA board emulator and establishes its network connection.
+     * * This constructor acts as the initialization phase for the FPGA. It stores the
+     * networking parameters, calculates the required bandwidth delays, and actively
+     * binds/connects the internal RAII socket (`UniqueFD`) to the target server.
+     * * @param id The unique identifier for this simulated board (e.g., 1 to 100).
+     * @param ip The target IP address or UNIX socket path of the DAQ server.
+     * @param port The target port on the DAQ server.
+     * @param proto The transport protocol to use ("tcp" or "udp"). Defaults to "tcp".
+     * @param internet_type The address family (AF_INET, AF_INET6, or AF_UNIX). Defaults to AF_INET.
+     * @param spill_duration_sec The length of the particle spill in seconds. Defaults to 4.0.
+     * @param frequency_Mhz The target transmission rate in MHz. Defaults to 4.
+     * @param buffer_size The number of 64-bit words to send in a single packet. Defaults to 1024.
+     * * @throws std::runtime_error If the socket creation or connection fails.
+     */
+    FakeFPG(const int id, const std::string &ip, int port, const std::string &proto = "tcp",
+            int internet_type = AF_INET, double spill_duration_sec = 4.0, double frequency_Mhz = 4,
             long long buffer_size = 1024);
 
-    // delete because it has a filedescriptor and we don't want to accidentally copy it
+    /**
+     * @brief Executes a single particle accelerator spill cycle.
+     * * This method simulates the high-throughput data generation of an FPGA during a beam spill.
+     * It performs the following sequence:
+     * 1. Transmits a predefined 64-bit PREAMBLE_START marker.
+     * 2. Enters a high-speed loop, generating and transmitting buffers of random 64-bit integers.
+     * 3. Uses the configured `frequency_Mhz_` to sleep between sends, rate-limiting the bandwidth.
+     * 4. Exits the loop after `spill_duration_sec_` has elapsed.
+     * 5. Transmits a predefined 64-bit PREAMBLE_END marker.
+     */
+    void run_spill();
+
+    // delete because it has a file descriptor and we don't want to accidentally copy it
     // also no need to have assignment operators as the sole purpose of this class
     // is to run a spill and then be destroyed. If you want to run multiple spills, just create
     // multiple instances of this class.
     FakeFPG(FakeFPG &&) = delete;
     FakeFPG(const FakeFPG &) = delete;
-
-    // TODO: Add move and copy assignment operators
     FakeFPG &operator=(FakeFPG &&) = delete;
     FakeFPG &operator=(const FakeFPG &) = delete;
 
     // Deconstructor
     ~FakeFPG();
-
-    // Main interface
-    void run_spill();
 
     // Add getters and setters for debugging
     int get_id() const
@@ -63,9 +85,12 @@ class FakeFPG
     // needs to do is send data to a specific destination. It doesn't need to modify these
     // parameters after construction.
     const int id_;
-    const std::string &ip_;
+    const double frequency_Mhz_; // Frequency in MHz, used to calculate the delay between sends to
+                                 // achieve the target bandwidth
+
+    const std::string ip_;
     const int port_;
-    const std::string &proto_;
+    const std::string proto_;
     const int internet_type_;
     const std::filesystem::path path_ = "/tmp/fpga_socket"; // Default path for UNIX domain socket
 
@@ -84,9 +109,6 @@ class FakeFPG
 
     // Using the UniqueFD pointer defined using RAII
     UniqueFD sockfd_;
-
-    // Private Utilities for setting up the connection
-    void _setup_connection();
 };
 
 #endif
